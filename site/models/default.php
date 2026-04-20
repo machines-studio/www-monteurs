4d57ccda9b244905c5b02cf50f112ef251088891
Arnaud Juracek <arnaud.juracek@gmail.com>
2026-04-20 14:06:43 +0200

    Fix isTranslated

--- site/models/default.php
+++ site/models/default.php
@@ -9,8 +9,8 @@ class DefaultPage extends Page {
   }
 
   public function isTranslated () : Field {
-    $fr = $this->content('fr')->blocks()->toBlocks()->toString();
-    $en = $this->content('en')->blocks()->toBlocks()->toString();
-    return new Field($this, 'isTranslated', $fr !== $en);
+    $fr = Str::unhtml($this->content('fr')->blocks()->toBlocks()->toHtml());
+    $en = Str::unhtml($this->content('en')->blocks()->toBlocks()->toHtml());
+    return new Field($this, 'isTranslated', trim($en) && $fr != $en);
   }
 }
